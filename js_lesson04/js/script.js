3dffd9aaf07accda28a36b618ce4153a10fb957c
Abigirl Muchineripi <muchineripiabby@gmail.com>
2025-04-15 15:20:34 +0200

    Math Methods

--- js_lesson04/js/script.js
+++ js_lesson04/js/script.js
@@ -5,32 +5,33 @@ console.log(Math.PI);
 // Expected output: 3.141592653589793
 
 // Math.trunc() method returns the integer part of a number by removing any fractional digits.
-console.log(Math.trunc(123.456));
-// Expected output: 123
+console.log(Math.trunc(2025.608));
+// Expected output: 2025
 
 // Math.round() method returns the value of a number rounded to the nearest integer.
-console.log(Math.round(0.9));
-// Expected output: 1
+console.log(Math.round(2.9));
+// Expected output: 3
 
 // Math.ceil() method always rounds up and returns the smaller integer greater than or equal to a given number.
-console.log(Math.ceil(7.004));
-// Expected output: 8
+console.log(Math.ceil(100.0001));
+// Expected output: 101 . has been rounded up
 
 // Math.floor() method always rounds down and returns the largest integer less than or equal to a given number.
-console.log(Math.floor(5.95));
-// Expected output: 5
+console.log(Math.floor(199.9999));
+// Expected output: 199, rounded down
 
 // Math.pow() method returns the value of a base raised to a power.
-console.log(Math.pow(7, 2));
-// Expected output: 49
+console.log(Math.pow(2, 2));
+// Expected output: 4 , it is to the power
 
 // Math.min() method returns the smallest of the numbers given as input parameters.
-console.log(Math.min(100, 7, 81));
-// Expected output: 7
+console.log(Math.min(3000, 1, 6700));
+// Expected output: 1
 
 // Math.max() method returns the largest of the numbers given as input parameters.
-console.log(Math.max(100, 7, 81));
-// Expected output: 100
+console.log(Math.max(1, 2, 90));
+// Expected output: 90
 
 // Math.random() method returns a random number between 0 (inclusive),  and 1 (exclusive).
 console.log(Math.random())
+// Expected output: keeps changing within the range 
