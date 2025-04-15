710370962302b2c4e0552802126e79c6830e9e19
Abigirl Muchineripi <muchineripiabby@gmail.com>
2025-04-15 14:45:04 +0200

    String Methods and Properties

--- js_lesson02/js/script.js
+++ js_lesson02/js/script.js
@@ -1,43 +1,48 @@
 // String Methods & Properties
 
 // The String object ("") is used to represent and manipulate a sequence of characters.
-const myVariable = "Mathematics";
+const myVariable = "JavaScript";
 console.log(myVariable);
-// Expected Output: Mathematics
+// Expected Output: JavaScript
 
 // The length property returns the number of code units in the string.
 console.log(myVariable.length);
-// Expected Output: 11
+// Expected Output: 10
 
 // The charAt() method returns a character from a string.
 console.log(myVariable.charAt(0));
-// Expected Output: M
+// Expected Output: J
 
 // The indexOf() method returns the position of the first occurrence of specified character(s) in a string.
-console.log(myVariable.indexOf("mat"));
-// Expected Output: 5
+console.log(myVariable.indexOf("ava"));
+// Expected Output: 1
 
 // The lastIndexOf() method returns the position of the last occurrence of a specified value in a string.
-console.log(myVariable.lastIndexOf("ema"));
-// Expected Output: 4
+console.log(myVariable.lastIndexOf("ipt"));
+// Expected Output: 7
 
 // The slice() method extracts a part of a string.
 console.log(myVariable.slice(5));
-// Expected Output: matics
+// Expected Output: cript
 
 // The toUpperCase() method converts a string to uppercase letters.
 console.log(myVariable.toUpperCase());
-// Expected Output: MATHEMATICS
+// Expected Output: JAVASCRIPT
 
 // TThe toLowerCase() method converts a string to lowercase letters.
 console.log(myVariable.toLowerCase());
-// Expected Output: mathematics
+// Expected Output: javascript
 
 // The includes() method returns true if a string includes a specified string.
-console.log(myVariable.includes("mat"));
+console.log(myVariable.includes("Scr"));
 // Expected Output: true
 
 // The split() method takes a pattern and divides a String into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.
-console.log(myVariable.split("e"));
-// Expected Output: 0: "Math" 1: "matics"
-// It was divided in half at "e" where it was targeted.
+console.log(myVariable.split("a"));
+// Expected Output: ['J', 'v', 'Script']
+/*0: "J"
+1: "v"
+2:"Script"
+length: 3
+*/
+// It was divided into 3 at "a" where it was targeted.
