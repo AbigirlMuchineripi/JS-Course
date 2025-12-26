4842d36e2e619c43b9da03b58ee077077c75e0ad
Abigirl Muchineripi <muchineripiabby@gmail.com>
2025-12-26 12:33:29 +0200

    Hello Message

--- js_lesson01/js/script.js
+++ js_lesson01/js/script.js
@@ -1,14 +1,27 @@
 // JavaScript Message
 console.log("Hello World!");
 // Expected Output: Hello World!
+console.log("I am still coding!");
+// Expected Output: I am still coding!
 
 // String. A JavaScript string stores a series of characters.
+console.log("Abigirl");
+// Expected Output: Abigirl
 console.log(typeof "Abigirl");
 // Expected Output: string
 
 // Number. JavaScript number values represent floating-point numbers.
 console.log(typeof 93);
 // Expected Output: number
+console.log(typeof 2.22);
+// Expected Output: number
+const n = 89
+console.log(typeof n)
+// Expected Output: number
+const m = 1.05;
+console.log(typeof m);
+// Expected Output: number
+
 
 // Boolean. JavaScript booleans can have one of two values: true or false.
 console.log(typeof true);
